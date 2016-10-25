01731f5d4568e75cef09ed13ccf466f8fe503f01
Max Battcher <me@worldmaker.net>
2016-10-25 00:11:37 -0400

    :candy: Pop the extra directory layer

--- NaNoWriMoAtGlance/NaNoWriMoAtGlance/livetiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-from views import *
-
-urlpatterns = patterns('',
-                       url('^glance', index),
-                       url('^notification/(?P<type>\w+)/(?P<source>.*)', notification),
-                       url('^$', TemplateView.as_view(template_name='livetiles/selector.html')),
-                      )
